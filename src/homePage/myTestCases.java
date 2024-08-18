4fa1c59f1f0cb883834ba88dbd51205e7b2877e2
MayaDughmosh <Maya_mousa02@oulook.com>
2024-08-18 05:48:58 +0300

    update

--- src/homePage/myTestCases.java
+++ src/homePage/myTestCases.java
@@ -1,6 +1,9 @@
 package homePage;
 
 import java.time.Duration;
+import java.time.LocalDate;
+import java.util.List;
+import java.util.Random;
 
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
@@ -15,6 +18,7 @@ public class myTestCases {
 	WebDriver driver=new ChromeDriver();
 	String AlmosaferUrl="https://global.almosafer.com/en";
 	String ExpectedDefaultLanguage="en";
+	Random rand=new Random();
 	@BeforeTest 
 	public void mySetup() {
 		driver.manage().window().maximize();
@@ -53,5 +57,48 @@ public class myTestCases {
 		
 		
 	}
+	@Test (priority=5)
+	public void CheckHotelSearchTabNotSelected() {
+		String ExpectedResultOfHotelsSelected="false";
+		WebElement HotelsTab=driver.findElement(By.id("uncontrolled-tab-example-tab-hotels"));
+		String ActualResult=HotelsTab.getAttribute("aria-selected");
+		Assert.assertEquals(ActualResult, ExpectedResultOfHotelsSelected);
+	}
+	
+	@Test (priority=6)
+	public void CheckDepature()
+	{
+		LocalDate todayDate=LocalDate.now();
+		;
+		int Today=todayDate.getDayOfMonth();
+		int Tomorrow=todayDate.plusDays(1).getDayOfMonth();
+		int TheDayAfterTomorrow=todayDate.plusDays(2).getDayOfMonth();
+	
+	
+List <WebElement> depatureAndArrivalDates=driver.findElements(By.className("LiroG"));
+
+String ActualDepatureDate=depatureAndArrivalDates.get(0).getText();
+
+String ActualArrivalDate=depatureAndArrivalDates.get(1).getText();
+	
+int ActualDepatureDateAsInt=Integer.parseInt(ActualDepatureDate);
+int ActualArivalDateAsInt=Integer.parseInt(ActualArrivalDate);
+
+Assert.assertEquals(ActualDepatureDateAsInt, Tomorrow);
+Assert.assertEquals(ActualArivalDateAsInt, TheDayAfterTomorrow);
+
+	
+		
+		
+	}
+	@Test (priority =7)
+	public void RandomlyChangeTheLanguage() {
+		String [] URLs= {"https://global.almosafer.com/en","https://global.almosafer.com/ar"};
+		int RandomUrl=rand.nextInt(URLs.length);
+		driver.get(URLs[RandomUrl]);
+	}
+	
+	
+	
 	
 }
